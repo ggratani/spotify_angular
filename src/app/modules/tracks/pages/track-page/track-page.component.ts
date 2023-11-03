40dfda64baf1a44d41c97adde21280f831b264b7
Gratani <gaston.gratani@accenture.com>
2023-11-03 15:24:37 -0300

    Sección 4

---
+++ src/app/modules/tracks/pages/track-page/track-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import * as dataRaw from '../../../../data/tracks.json'
+import { TrackModel } from '@core/models/tracks.model';
+
+@Component({
+  selector: 'app-track-page',
+  templateUrl: './track-page.component.html',
+  styleUrls: ['./track-page.component.css']
+})
+
+export class TrackPageComponent implements OnInit {
+
+  mockTrackList: Array<TrackModel> = [
+
+  ]
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default
+    this.mockTrackList = data;
+  }
+}
--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
   styleUrls: ['./media-player.component.css']
 })
-export class MediaPlayerComponent {
+export class MediaPlayerComponent implements OnInit {
 
+  mockCover: TrackModel = {
+    cover:'https://th.bing.com/th/id/OIP.fuh_05g6z3xRv_JAxxzD1AHaEo?pid=ImgDet&rs=1',
+    album:'Perro',
+    name:'Perro nombre',
+    url:'http://localhost/track.mp3',
+    _id: 1
+  }
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    
+  }
 }
